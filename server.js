46fd3ca710c7d2e7918e24b9e2186dcc9ea79eb0
olekunger <ounger@inbox.ru>
2019-07-03 17:47:41 +0300

    -

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/ungers-demo-auth'));
+app.use(express.static(__dirname + '/dist/ungers-auth-demo'));
 app.get('/*', (req,res)=>{
-  res.sendFile(path.join(__dirname, '/dist/ungers-demo-auth/index.html'))
+  res.sendFile(path.join(__dirname, '/dist/ungers-auth-demo/index.html'))
 })
 app.listen(process.env.PORT || 8080);
